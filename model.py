5b5220e7a5d999c36844074cef69c905687a5c31
Arushi <aarushie2004@gmail.com>
2020-08-24 15:55:12 -0400

    added updated model

--- model.py
+++ model.py
@@ -3,24 +3,26 @@ import torch.nn as nn
 
 
 class RNN(nn.Module):
-    def __init__(self, vocab_size, num_classes, embed_dim=50, hidden_size=100):
+    def __init__(self, vocab_size, num_classes, embedding_dim=50, hidden_dim=100):
         super(RNN, self).__init__()
 
-        self.embedding = nn.Embedding(vocab_size, embed_dim)
-        self.lstm = nn.LSTM(embed_dim, hidden_size, num_layers=1)
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=1, batch_first=True)
         self.relu = nn.ReLU(inplace=False)
-        self.fc = nn.Linear(hidden_size, num_classes)
+        self.fc = nn.Linear(hidden_dim, num_classes)
 
     def forward(self, input):
         #input_dim = batch*seq_len(length of name)
-        embedded = self.embedding(input) #embedding-dim : batch*seq_len*embed_dim
-        B, L, D = embedded.size()
-        embedded = embedded.view(L, B, D) #[20, 5, 50]
+
+        embedded = self.embedding(input) #embedding-dim(input dimension) : batch * seq_len * embed_dim
+        #B, L, D = embedded.size()
+        #embedded = embedded.view(L, B, D) #[20, 5, 50]
 
         _, (h_n, c_n) = self.lstm(embedded)
         #print(h_n.shape) #[1, 5, 100]
         hidden = torch.squeeze(h_n)
-        linear_output = self.fc(self.relu(hidden))
+        #linear_output = self.fc(self.relu(hidden))
+        linear_output = self.fc(hidden)
         #seq_len * batch * input_size -> (seq_len, batch, hidden_size), h_n, c_n
         return linear_output
 
